071dd1d01c9f1790d97c9bff1f7449876dd5a7b4
Arnaud Juracek <arnaud.juracek@gmail.com>
2026-01-12 13:47:38 +0100

    Release 0.1.0

---
+++ public/dist/assets/default-BttZbSWQ.js
@@ -0,0 +1,2 @@
+import{B as t}from"./index-p6iB7gdc.js";const f=new t("default",{beforeEnter:function({next:e}){},afterEnter:function({next:e}){},beforeLeave:function(){},afterLeave:function(){}});export{f as default};
+//# sourceMappingURL=default-BttZbSWQ.js.map
--- public/dist/assets/default-D7uZ0JnF.js
+++
@@ -1,2 +0,0 @@
-import{B as t}from"./index-Dpv2Pf8m.js";const f=new t("default",{beforeEnter:function({next:e}){},afterEnter:function({next:e}){},beforeLeave:function(){},afterLeave:function(){}});export{f as default};
-//# sourceMappingURL=default-D7uZ0JnF.js.map
