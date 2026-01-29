f8b833c77b2e16bda48f575aa37ed88a989e7cc8
Stéphane Buellet <bust@users.noreply.github.com>
2026-01-29 17:49:08 +0100

    Update Home

--- site/snippets/views/home.php
+++ site/snippets/views/home.php
@@ -1,26 +1,25 @@
 <?php
+
+  $activities = $site->index()->listed()->filterBy('intendedTemplate', 'activity');
+  $home = $site->index()->unlisted()->filterBy('intendedTemplate', 'home')->first();
+  $partners = new Collection(); // TODO[panel]
+
   // TODO[panel]
-  $subtitle = 'Construire juste';
+  $subtitle =  $home->heroSubtitle();
 
   // TODO[panel]
   $descriptionPattern = asset('assets/background-green.jpg');
-  $description = <<<HTML
-    Contractant général pour la construction de bâtiments industriels,
-    logistiques et tertiaires clé en main
-  HTML;
+  $description = $home->heroDescription();
 
   // TODO[panel]
-  $text = <<<HTML
-    <p><b>C13N Group est un contractant général</b> spécialisé en immobilier d'entreprise de grande envergure : usines, sites logistiques, sièges sociaux, bureaux. Dirigé par un architecte, le groupe se démarque par la qualité architecturale de ses réalisations, appuyées sur des compétences solides en ingénierie et en suivi de chantiers complexes. C13N Group est l'interlocuteur unique pour la conception et la réalisation de vos projets, prix et résultat garantis.</p>
-  HTML;
+  $text = $home->keyFiguresDescription();
 
-  $activities = $site->index()->listed()->filterBy('intendedTemplate', 'activity');
-  $partners = new Collection(); // TODO[panel]
 ?>
+
 
 <?php snippet('html/image', [
   // TODO[panel]
-  'image' => $site->images()->shuffle()->first(),
+  'image' => $home->cover()->toFiles()->shuffle()->first(),
   'lazyload' => false, // Improve LCP
   'attributes' => ['class' => 'hero']
 ]) ?>
@@ -69,15 +68,39 @@
 ], slots: true) ?>
   <?php slot('content') ?>
     <h2>
-      <?php // TODO[panel] ?>
-      Nos domaines d’expertise
+      <?=  $home->skillsSubtitle(); ?>
     </h2>
 
     <div class='activities cols-<?= $activities->count() ?>'>
-      <?php foreach ($activities as $page) : ?>
-        <a href='<?= $page->url() ?>'>
-          <?= asset('assets/' . $page->slug() . '.svg') ?>
-          <span><?= $page->title() ?></span>
+      <?php foreach ($home->skillsItems()->toStructure() as $index => $item) : ?>
+        <?php
+          $skillImage = $item->skillImage()->toFile()?->url();
+          $skillTitle = $item->skillTitle();
+          $skillUrl = $item->skillUrl()->toPage();
+          $patternId = "pattern-skill-{$index}";
+        ?>
+
+        <a href='<?= $skillUrl?->url() ?? '#' ?>'>
+          <svg width="256" height="356" viewBox="0 0 256 356" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <pattern id="<?= $patternId ?>" patternContentUnits="objectBoundingBox" width="1" height="1">
+                <image href="<?= $skillImage ?>" x="0" y="0" width="1" height="1" preserveAspectRatio="xMidYMid slice"/>
+              </pattern>
+            </defs>
+            <?php
+              // Charger le SVG depuis assets/skill{index}.svg
+              $svgPath = asset("assets/skill{$index}.svg")->root();
+              if (file_exists($svgPath)) {
+                $svgContent = file_get_contents($svgPath);
+                // Extraire le path du SVG (en supposant qu'il y a un path principal)
+                preg_match('/<path[^>]*d="([^"]*)"[^>]*\/?>/', $svgContent, $matches);
+                if (!empty($matches[1])) {
+                  echo '<path d="' . $matches[1] . '" fill="url(#' . $patternId . ')" stroke="#FAF1E3" stroke-width="2" stroke-miterlimit="10"/>';
+                }
+              }
+            ?>
+          </svg>
+          <span><?= $skillTitle ?></span>
         </a>
       <?php endforeach ?>
     </div>
@@ -89,8 +112,7 @@
 ], slots: true) ?>
   <?php slot('content') ?>
     <h2>
-      <?php // TODO[panel] ?>
-      Ils nous ont fait confiance
+      <?= $home->partnersSubtitle() ?>
     </h2>
 
     <div class='partners cols-<?= $partners->count() ?>'>
