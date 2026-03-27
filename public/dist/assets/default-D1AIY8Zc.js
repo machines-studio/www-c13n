9da817d9ecd81927b39e17f308a25d97edb5e3eb
Stéphane Buellet <bust@users.noreply.github.com>
2026-03-27 12:18:32 +0100

    v0.3.1

---
+++ public/dist/assets/default-D1AIY8Zc.js
@@ -0,0 +1,2 @@
+import{B as t}from"./index-F6gk7tfi.js";const f=new t("default",{beforeEnter:function({next:e}){},afterEnter:function({next:e}){},beforeLeave:function(){},afterLeave:function(){}});export{f as default};
+//# sourceMappingURL=default-D1AIY8Zc.js.map
